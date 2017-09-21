1f417a8545b2da1ee252843ce7df0e1929d9b8a3
jeb192004 <jeb192004@Gmail.com>
2017-09-20 20:05:24 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,7 @@
-
-// Contents of an element
-var clipboard = new Clipboard('.btn', {
-    target: function() {
-        return document.getElementById('textbox_id').value;
-    }
-});
-clipboard.on('success', function(e) {
-    console.info('Action:', e.action);
-    console.info('Text:', e.text);
-    console.info('Trigger:', e.trigger);
-
-    e.clearSelection();
-});
-
-clipboard.on('error', function(e) {
-    console.error('Action:', e.action);
-    console.error('Trigger:', e.trigger);
-});
-
-
-$(document).ready(function() {
-    var max_fields      = 10;
-    var wrapper         = $(".container1");
-    var add_button      = $(".add_form_field");
-  
-    var x = 1;
-    $(add_button).click(function(e){
-       var latlon1 = document.getElementById('latlon1').select();
-        document.execCommand('copy');
-        //e.preventDefault();
-        //if(x < max_fields){
-          //  x++;
-            //$(wrapper).append('<div><input type="text" name="mytext[]"/><a href="#" class="delete">Delete</a></div>'); //add input box
-        }
-  else
-  {
-  alert('You Reached the limits')
-  }
-    });
-  
-    $(wrapper).on("click",".delete", function(e){
-        e.preventDefault(); $(this).parent('div').remove(); x--;
-    })
-});
+// Copy to clipboard example
+document.querySelector("#visible-button").onclick = function() {
+  // Select the content
+  document.querySelector("#visible-input").select();
+  // Copy to the clipboard
+  document.execCommand('copy');
+};
