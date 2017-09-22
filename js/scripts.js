8b743d4f385376d6e4637ed4d8040ff1ba087b4b
jeb192004 <jeb192004@gmail.com>
2017-09-22 10:56:27 -0400

    geojson maker

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
 // Copy to clipboard example
 document.querySelector("#visible-button").onclick = function() {
   // Select the content
-  document.querySelector("#visible-input").select();
+  var orvTH = document.querySelector("#visible-input").value;
+ var orvTHLatLon = orvTH.split(',', 2);
+ var orvTHLat = orvTHLatLon[0];
+  var orvTHLon = orvTHLatLon[1];
+	var orvthMapdata = orvTHLon +", " + orvTHLat;
+	document.querySelector("#new-cord").value = orvthMapdata;
+	document.querySelector("#new-cord").select();
+	alert(orvthMapdata);
   // Copy to the clipboard
   document.execCommand('copy');
 };
