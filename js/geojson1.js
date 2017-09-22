ea2ec6e92b0420d700e1ee2c82333c81e8009c58
jeb192004 <jeb192004@Gmail.com>
2017-09-22 17:27:59 -0400

    Update geojson1.js

--- js/geojson1.js
+++ js/geojson1.js
@@ -7,22 +7,22 @@ document.getElementById("myform2").style.display='none';
 		var lat = cord[0];
 		var lon = cord[1];
 		var lonlat = lon+','+lat;
-		document.getElementById("in").value="";
-	        document.getElementById("in2").value="";
+		
         $('ul.list').append('<li>{ "type": "Feature", "properties": { "Name": "' +
 		val +'", "Description": "" }, "geometry": { "type": "Point", "coordinates":['+ lonlat + ',0.0 ] } }</li>');
        
-		
+		e.preventDefault();
     });
 	$('#myform2').submit(function(e){
         var val = $(this).find('#in').val();
 		var val2 = $(this).find('#in2').val();
-		document.getElementById("in").value="";
-	    document.getElementById("in2").value="";
+		
         $('ul.list').append('<li>{ "type": "Feature", "properties": { "Name": "' +
 		val +'", "Description": "" }, "geometry": { "type": "Point", "coordinates":['+ val2 + ',0.0 ] } }</li>');
-       
+       e.preventDefault();
     });
+	document.getElementById("in").value="";
+	        document.getElementById("in2").value="";
 })(jQuery);
 
 document.getElementById("dwn-btn").addEventListener("click", function(){
